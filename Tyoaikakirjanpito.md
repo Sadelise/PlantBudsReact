41e7dcba8150e6fe7ff2a7f777702da153dd9f85
Sadelise <sharhio@hotmail.com>
2018-09-16 15:56:47 +0300

    List filter

--- Tyoaikakirjanpito.md
+++ Tyoaikakirjanpito.md
@@ -5,3 +5,4 @@
 | 06.09. | 1 | Projektin luonti |
 | 08.09. | 6 | Koodauksen aloittelua React-oppeja kertaamalla |
 | 09.09. | 8 | Ulkoasun parannus, parempi lomake, firebase-konfigurointi, backend-palveluita ja niiden ongelmia|
+| 15.09 | 3 | rakennepohdintaa, deletointi |
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,30 +3,52 @@ import Plant from './Plant'
 import { connect } from 'react-redux'
 import Grid from '@material-ui/core/Grid';
 import { deletePlant } from '../services/plantService'
-// import TextField from '@material-ui/core/TextField';
+import TextField from '@material-ui/core/TextField';
 
 class PlantList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            filterBy: ''
+        }
+    }
 
     deletePlant = (plant) => {
         this.props.deletePlant(plant.id)
     }
 
+    onSearchInputChange = (event) => {
+        this.setState({ filterBy: event.target.value })
+    }
+
+    filterByName = (name) => {
+        if (name !== undefined) {
+            this.setState({ filterBy: name })
+        }
+    }
+
     render() {
+        const plantsToShow =
+            this.state.filterBy === '' ?
+                this.props.plants :
+                this.props.plants.filter(plant => plant.finnishName.toLowerCase().indexOf(this.state.filterBy.toLowerCase()) > -1)
+
         return (
             <div>
                 <div>
-                    {/* <TextField style={{ padding: 24 }}
+                    <TextField style={{ padding: 24 }}
                         id="searchInput"
                         placeholder="Search for Plants"
                         margin="normal"
                         onChange={this.onSearchInputChange}
-                    /> */}
+                    />
 
                     <Grid container spacing={24} style={{ padding: 24 }}>
-                        {this.props.plants.map(plant => (
+                        {plantsToShow.map(plant => (
                             <Grid key={plant.id} item xs={12} sm={6} lg={4} xl={3}>
                                 <Plant plant={plant}
-                                    deletePlant={this.deletePlant} />
+                                    deletePlant={this.deletePlant}
+                                    filterByName={this.filterByName} />
                             </Grid>
                         ))}
                     </Grid>
