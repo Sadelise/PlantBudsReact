23a0c6b3eaa788b4622eee22cdbed3dc3bc60def
Sadelise <sharhio@hotmail.com>
2018-12-28 12:24:53 +0000

    ignore env, env variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,14 @@
 import * as firebase from "firebase";
-import { FirebaseConfig } from "../config/keys";
+//import { FirebaseConfig } from "../config/keys";
+
+const FirebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+};
 
 firebase.initializeApp(FirebaseConfig);
 const databaseRef = firebase.database().ref();
